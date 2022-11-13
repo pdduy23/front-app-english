9f1eebff9f5f6d5dc08d382e4e0b799b2f6efb4c
pdduy23 <cuong234110@gmail.com>
2022-11-13 21:15:18 +0700

    Add new word.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let word = []
 
 const getWord = async () => {
-    await fetch('http://localhost:3000/api/getword', {
+    await fetch('https://server-app-english.onrender.com/api/getword', {
         method: 'GET'
     }).then(res => res = res.json()).then(res =>
         word = res.vocab
@@ -26,4 +26,28 @@ const getDefine = () => {
         text = text + `<td>${word[i].define}</td><br/>`
     }
     document.getElementById("define").innerHTML = text
+}
+
+const addWord = async () => {
+    const textDefine = document.getElementById("textDefine")
+    const textWord = document.getElementById("textWord")
+
+    const body = {
+        "define": textDefine.value.toString(),
+        "word": textWord.value.toString(),
+    }
+
+    await fetch('http://localhost:3000/api/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+    }).then(res => res.json()).then(res => {
+        if (res.success == true) {
+            textDefine.value = ""
+            textWord.value = ""
+        }
+    })
+        .catch(err => console.log(err))
 }
